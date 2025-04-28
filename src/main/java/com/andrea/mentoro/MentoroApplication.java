72242227fdbad543740e941ed87189e1d68d52ab
{ andie } <124734884+andie-dm@users.noreply.github.com>
2025-04-28 14:09:20 -0600

    initial commit

---
+++ src/main/java/com/andrea/mentoro/MentoroApplication.java
@@ -0,0 +1,13 @@
+package com.andrea.mentoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrea/mentoro/MentoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrea.mentoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
